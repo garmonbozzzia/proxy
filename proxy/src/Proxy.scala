cc4538b93e5a985696fd3604d36e894c8b6a7cb9
Yury Ignatev <yurabuy@yandex.ru>
2018-04-25 02:15:52 +0300

    akka cache

--- proxy/src/Proxy.scala
+++ proxy/src/Proxy.scala
@@ -1,17 +1,19 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding.Get
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.CachingDirectives.cache
+import akka.http.scaladsl.server.{RequestContext, RouteResult}
 import akka.stream.ActorMaterializer
-
-import scala.io.StdIn
-import akka.http.scaladsl.client.RequestBuilding.Get
 import akka.util.ByteString
 
 import scala.concurrent.Future
+import scala.io.StdIn
 
 //80.211.27.151/ru/schedules/schdullabha
 object Proxy extends App {
+  val calendar = java.util.Calendar.getInstance
   implicit class Traceable[A] (val obj: A) extends AnyVal {
     def traceWith[B](f: A => B ): A = { println(f(obj)); obj}
     def trace[U](u: => U): A = traceWith(_ => u)
@@ -23,35 +25,49 @@ object Proxy extends App {
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
-  case class Data(bs: ByteString, ct: ContentType)
-  val cache = scala.collection.mutable.Map.empty[Uri, (Long,Data)]
-  def requestAndSave(uri: Uri) = for {
-    response <- Http().singleRequest(Get(uri))
-    data <- response.entity.dataBytes.runFold(ByteString.empty)(_ ++ _)
-    obj = Data(data, response.entity.contentType)
-    _ = cache(uri) = (System.currentTimeMillis, obj)
-  } yield obj
+  import akka.http.caching.LfuCache
+  import akka.http.caching.scaladsl.{Cache, CachingSettings}
+  //#caching-directives-import
+  //#always-cache
+  //#cache
+  import akka.http.scaladsl.server.directives.CachingDirectives._
 
-  def cached(uri: Uri): Future[Data] = cache.get(uri).fold{
-    requestAndSave(uri)
-  }{
-    case (t, data) => if (System.currentTimeMillis - t > 5000) requestAndSave(uri) else Future.successful(data)
+  import scala.concurrent.duration._
+
+  val defaultCachingSettings = CachingSettings(system)
+  val lfuCacheSettings =
+    defaultCachingSettings.lfuCacheSettings
+      .withInitialCapacity(25)
+      .withMaxCapacity(50)
+      .withTimeToLive(20.seconds)
+      .withTimeToIdle(10.seconds)
+  val cachingSettings = defaultCachingSettings.withLfuCacheSettings(lfuCacheSettings)
+  val lfuCache: Cache[Uri, RouteResult] = LfuCache(cachingSettings)
+  val keyerFunction: PartialFunction[RequestContext, Uri] = {
+    case r: RequestContext ⇒ r.request.uri
   }
+
 
   val route =
     path(Remaining) { path =>
-      extractUri { uri =>
-        extractRequest { request =>
-          get {  onSuccess(cached(uri.withHost("www.dhamma.org").withScheme("https").trace)) {
-              data => complete(HttpEntity(data.ct, data.bs))
+      extractClientIP { ip =>
+        if (path == "/ru/schedules/schdullabha") calendar.getTime.traceWith(t => s"[$t: $ip]")
+        extractUri { uri =>
+          get {
+            cache(lfuCache, keyerFunction)(
+              onSuccess(Http().singleRequest(Get(uri.withHost("www.dhamma.org").withScheme("https").withPort(0)))) {
+              response =>
+                onSuccess(response.entity.dataBytes.runFold(ByteString.empty)(_ ++ _)) {
+                  data => complete(HttpEntity(response.entity.contentType, data))
+                }
               }
-            }
+            )
           }
         }
       }
+    }
 
-
-  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 80)
+  val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
   println(s"Server online at http://localhost:8080/ru/schedules/schdullabha\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
